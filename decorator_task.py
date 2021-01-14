d5f0da3cf40b0627184d84fe44d4f343f9e69a30
Intma1n <forestraccoon999@gmail.com>
2021-01-14 03:55:32 +0200

    not done yet

--- decorator_task.py
+++ decorator_task.py
@@ -3,23 +3,23 @@ from abc import ABC, abstractmethod
 
 class Hero:
     def __init__(self):
-        self.positive_effects = []
-        self.positive_effects = []
+        self.positive_effects =[]
+        self.negative_effects = []
         self.stats = {
-            "HP": 128,
-            "MP": 42,
-            "SP": 100,
-            "Strength": 15,
-            "Perception": 4,
-            "Endurance": 8,
-            "Charisma": 2,
-            "Intelligence": 3,
-            "Agility": 8,
-            "Luck": 1
+            "HP": 128, #health points
+            "MP": 42, #magic points
+            "SP": 100, #skill points
+            "Strength": 15, #сила
+            "Perception": 4, #восприятие
+            "Endurance": 8, #выносливость
+            "Charisma": 2, #харизма
+            "Intelligence": 3, #ителлект
+            "Agility": 8, #ловкость
+            "Luck": 1 #удача
         }
 
     def get_positive_effects(self):
-        return self.positive_effets.copy()
+        return self.positive_effects.copy()
 
     def get_negative_effects(self):
         return self.negative_effects.copy()
@@ -29,51 +29,89 @@ class Hero:
 
 
 class AbstractEffect(Hero):
+    def __init__(self, base):
+        super().__init__()
+        self.base = base
+
     def get_positive_effects(self):
-        pass
+        self.base.get_positive_effects()
+
+    def get_stats(self):
+        self.base.get_stats()
 
     def get_negative_effects(self):
-        pass
-
-
-class AbstractNegative(AbstractEffect):
-    def __init__(self, effect):
-        self.effect = effect
-
-    def get_negative_effects(self):
-        pass
+        self.base.get_negative_effects()
 
 
 class AbstractPositive(AbstractEffect):
-    def __init__(self, effect):
-        self.effect = effect
-
     def get_positive_effects(self):
-        pass
+        self.base.get_positive_effects()
+
+    def get_stats(self):
+        self.base.get_stats()
+
+
+class AbstractNegative(AbstractEffect):
+    def get_positive_effects(self):
+        self.base.get_positive_effects()
+
+    def get_stats(self):
+        self.base.get_stats()
 
 
 class Berserk(AbstractPositive):
-    pass
+    def get_positive_effects(self):
+        self.positive_effects.append('Berserk')
+
+    def get_stats(self):
+        pass
 
 
 class Blessing(AbstractPositive):
-    pass
+    def get_positive_effects(self):
+        self.positive_effects.append('Blessing')
+
+    def get_stats(self):
+        pass
 
 
 class Weakness(AbstractNegative):
-    pass
+    def get_negative_effects(self):
+        self.positive_effects.append('Weakness')
+
+    def get_stats(self):
+        pass
 
 
 class EvilEye(AbstractNegative):
-    pass
+    def get_negative_effects(self):
+        self.positive_effects.append('EvilEye')
+
+    def get_stats(self):
+        pass
 
 
 class Curse(AbstractNegative):
-    pass
+    def get_negative_effects(self):
+        self.positive_effects.append('Curse')
+
+    def get_stats(self):
+        pass
 
 
 def main():
-    pass
+    hero = Hero()
+    print(hero.get_stats())
+    print(hero.stats)
+    print(hero.get_negative_effects())
+    print(hero.get_positive_effects())
+
+    print()
+
+    brs1 = Berserk(hero)
+    print(brs1.get_stats())
+    print(brs1.get_negative_effects())
+    print(brs1.get_positive_effects())
 
 
 if __name__ == '__main__':
